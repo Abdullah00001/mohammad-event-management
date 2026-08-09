bd97e1de1c5e275c7245af77f09196f9adad6327
Abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2026-08-09 14:30:07 +0600

    refactor: remove obsolete migration files for lastReadAt and isPresent columns

--- prisma/migrations_backup/20260720082542_add_last_read_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "ConversationParticipant" ADD COLUMN     "lastReadAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- prisma/migrations_backup/20260724104221/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "EventParticipants" ADD COLUMN     "isPresent" BOOLEAN NOT NULL DEFAULT false;
